979c382d430c407b25d97364905a0ec89bc6a94f
Daniel Hernán Vargas Osorio <dhernan.vargas@gmail.com>
2018-05-21 19:12:03 -0500

    Variables página Derechos

--- js/indicadores.js
+++ js/indicadores.js
@@ -1,4 +1,5 @@
 //---- Variables
+var pathAPI = "https://datosabiertos.unam.mx/api/alice/";
 var datosDer = [];
 var acumula = [];
 var tablaInd =  '';
@@ -10,9 +11,10 @@ var c = url.searchParams.get("codigo");
 
 $(document).ready(function() {
     
+    // Aquí se cargan los datos de los derechos
     $.ajax({
 		  type: 'GET',
-		  url: "https://datosabiertos.unam.mx/api/alice/search?q=guid:/"+ c +"/",
+		  url: pathAPI + "search?q=guid:/"+ c +"/",
 		  data: {},
 		  success: function( data, textStatus, jqxhr ) {      
             datosDer = data['results']['records'];
@@ -52,113 +54,52 @@ $(document).ready(function() {
     }
     
     
-var Utf8 = {
- 
+    var Utf8 = {
+        // public method for url encoding
+        encode : function (string) {
+            string = string.replace(/\r\n/g,"\n");
+            var utftext = "";
 
-    // public method for url encoding
-
-    encode : function (string) {
-
-        string = string.replace(/\r\n/g,"\n");
-
-        var utftext = "";
- 
-
-        for (var n = 0; n < string.length; n++) {
- 
-
-            var c = string.charCodeAt(n);
- 
-
-            if (c < 128) {
-
-                utftext += String.fromCharCode(c);
-
+            for (var n = 0; n < string.length; n++) {
+                var c = string.charCodeAt(n);
+                if (c < 128) {
+                    utftext += String.fromCharCode(c);
+                }else if((c > 127) && (c < 2048)) {
+                    utftext += String.fromCharCode((c >> 6) | 192);
+                    utftext += String.fromCharCode((c & 63) | 128);
+                }else {
+                    utftext += String.fromCharCode((c >> 12) | 224);
+                    utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                    utftext += String.fromCharCode((c & 63) | 128);
+                }
             }
+            return utftext;
+        },
 
-            else if((c > 127) && (c < 2048)) {
-
-                utftext += String.fromCharCode((c >> 6) | 192);
-
-                utftext += String.fromCharCode((c & 63) | 128);
-
+        // public method for url decoding
+        decode : function (utftext) {
+            var string = "";
+            var i = 0;
+            var c = c1 = c2 = 0;
+            while ( i < utftext.length ) {
+                c = utftext.charCodeAt(i);
+                if (c < 128) {
+                    string += String.fromCharCode(c);
+                    i++;
+                }else if((c > 191) && (c < 224)) {
+                    c2 = utftext.charCodeAt(i+1);
+                    string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                    i += 2;
+                } else {
+                    c2 = utftext.charCodeAt(i+1);
+                    c3 = utftext.charCodeAt(i+2);
+                    string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                    i += 3;
+                }
             }
-
-            else {
-
-                utftext += String.fromCharCode((c >> 12) | 224);
-
-                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-
-                utftext += String.fromCharCode((c & 63) | 128);
-
-            }
- 
-
+            return string;
         }
- 
-
-        return utftext;
-
-    },
- 
-
-    // public method for url decoding
-
-    decode : function (utftext) {
-
-        var string = "";
-
-        var i = 0;
-
-        var c = c1 = c2 = 0;
- 
-
-        while ( i < utftext.length ) {
- 
-
-            c = utftext.charCodeAt(i);
- 
-
-            if (c < 128) {
-
-                string += String.fromCharCode(c);
-
-                i++;
-
-            }
-
-            else if((c > 191) && (c < 224)) {
-
-                c2 = utftext.charCodeAt(i+1);
-
-                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-
-                i += 2;
-
-            }
-
-            else {
-
-                c2 = utftext.charCodeAt(i+1);
-
-                c3 = utftext.charCodeAt(i+2);
-
-                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-
-                i += 3;
-
-            }
- 
-
-        }
- 
-
-        return string;
-
     }
- 
-}
     
     
     function indicadorCuali(data){
