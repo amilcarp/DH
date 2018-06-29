55050cf609ac44cff4f1934cf36ca442cf8a8fbe
Daniel Hernán Vargas Osorio <dhernan.vargas@gmail.com>
2018-06-28 21:39:57 -0500

    Clean functions

--- js/indicadores.js
+++ js/indicadores.js
@@ -786,6 +786,7 @@ $(document).ready(function() {
         
         
         if(clas === "entidad" || clas === "Entidad Federativa"){
+        //if(clas === "1" || clas === "En"){
             
         var inf = [];
             
@@ -894,61 +895,6 @@ $(document).ready(function() {
 //        }//fin del for
         return contenido;
     }
-    
-    function exportTableToCSV($table, filename) {
-            var $rows = $table.find('tr:has(td)'),
-                tmpColDelim = String.fromCharCode(11), // vertical tab character
-                tmpRowDelim = String.fromCharCode(0), // null character
-                // actual delimiter characters for CSV format
-                colDelim = '","',
-                rowDelim = '"\r\n"',
-
-                // Grab text from table into CSV formatted string
-                csv = '"' + $rows.map(function(i, row) {
-                    var $row = $(row),
-                        $cols = $row.find('td');
-
-                    return $cols.map(function(j, col) {
-                        var $col = $(col),
-                            text = $col.text();
-
-                        return text.replace(/"/g, '""'); // escape double quotes
-
-                    }).get().join(tmpColDelim);
-
-                }).get().join(tmpRowDelim)
-                .split(tmpRowDelim).join(rowDelim)
-                .split(tmpColDelim).join(colDelim) + '"';
-
-            // Deliberate 'false', see comment below
-            if (false && window.navigator.msSaveBlob) {
-
-                var blob = new Blob([decodeURIComponent(csv)], {
-                    type: 'text/csv;charset=utf8'
-                });
-
-                window.navigator.msSaveBlob(blob, filename);
-            } else if (window.Blob && window.URL) {
-                var blob = new Blob([csv], {
-                    type: 'text/csv;charset=utf8'
-                });
-                var csvUrl = URL.createObjectURL(blob);
-
-                $(this)
-                    .attr({
-                        'download': filename,
-                        'href': csvUrl
-                    });
-            } else {
-                var csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
-                $(this)
-                    .attr({
-                        'download': filename,
-                        'href': csvData,
-                        'target': '_blank'
-                    });
-            }
-        }
 
         function deselect(e) {
             $('.pop').slideFadeToggle(function() {
